e6042cb83a67b4436c6f1e931ff507580b18fbfb
rickytranmer <rwtranmer@gmail.com>
2018-01-22 11:20:00 -0700

    completed up to step 6

--- imperial-starfleet/src/app/info-window/info-window.component.ts
+++ imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-info-window',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoWindowComponent implements OnInit {
 
-  constructor() { }
+	dataBanks = {};
+
+  constructor(private http: Http) { }
+
+  findTurret(turret) {
+  	console.log(`finding ${turret}`);
+  	this.http.get('https://swapi.co/api/people/?search='+turret)
+  		.toPromise()
+  		.then( (res)=>  console.log(res.json().results) );
+  }
 
   ngOnInit() {
   }
